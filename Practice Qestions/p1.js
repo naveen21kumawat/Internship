f3b2312aa1a4b21b2c0a4acba924eee94d10bb98
Naveen Kumawat <naveendimond77@gmail.com>
2025-07-01 20:18:52 +0530

    Add initial implementation of JavaScript functions and object manipulation examples

---
+++ Practice Qestions/p1.js
@@ -0,0 +1,36 @@
+
+const Name = "Naveen";
+// Name = "Naveen Kumawat";  // This line would cause an error because Name is a constant
+console.log(Name);
+let Sum = (n1, n2) => {
+  return n1 + n2;
+};
+
+console.log(Sum(10, 20));
+
+const Person = {
+  name: "Naveen",
+  age: 19,
+};
+const { name, age } = Person;
+console.log(name, age);
+
+const array = [5, 6];
+
+const [first, second] = array;
+console.log(first, second);
+
+array.map((item) => {
+  console.log(item);
+});
+
+const newDtta = array.filter((item) => {
+  return item > 5;
+});
+console.log(newDtta);
+
+const newarray = [...array, 7, 8, 9];
+console.log(newarray);
+
+const newObject = { ...Person, city: "Jaipur" };
+console.log(newObject);
